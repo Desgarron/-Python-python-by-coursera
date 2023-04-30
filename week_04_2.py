c1c0b8357dfcf10ddc4a95e1e73dd74160d00beb
Yaroslav_Nahornyi <nahornyi.y@gmail.com>
2023-05-01 00:31:12 +0300

    update week_04

--- week_04_2.py
+++ week_04_2.py
@@ -23,6 +23,30 @@ class Account:
         self.commission = commission
 
 
-values = Account(0.1)
-values.amount = 100
-print(values.amount)
+class ValueV2:
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, obj, obj_type):
+        return obj.__dict__[self.name]
+
+    def __set__(self, obj, value):
+        obj.__dict__[self.name] = value * (1 - obj.commission)
+
+
+class AccountV2:
+    amount = ValueV2()
+
+    def __init__(self, commission):
+        self.commission = commission
+
+
+if __name__ == '__main__':
+    values = Account(0.1)
+    values.amount = 100
+    print('Account =', values.amount)
+
+    values = AccountV2(0.1)
+    values.amount = 100
+    print('AccountV2 =', values.amount)
